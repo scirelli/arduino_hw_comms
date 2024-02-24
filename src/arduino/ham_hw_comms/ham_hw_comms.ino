5cbc0c9c11328bbc7f3812501768a736f8267adc
Steve Cirelli <scirelli+git@gmail.com>
2024-02-24 15:47:58 -0500

    Fixed byte stream so order in buffer works out.

--- src/arduino/ham_hw_comms/ham_hw_comms.ino
+++ src/arduino/ham_hw_comms/ham_hw_comms.ino
@@ -7,7 +7,8 @@
 
 const unsigned long HEADER = 0xD00000DE;
 const unsigned long FOOTER = 0xDEADBEAF;
-
+//const unsigned long HEADER = 0x41424344;
+//const unsigned long FOOTER = 0x45464748;
 unsigned int intValue;
 unsigned long longValue;
 
@@ -17,36 +18,25 @@ void setup() {
 
 void loop() {
   sendBinary(HEADER);
-  Serial.print("My message!");
+  Serial.print("ABCDEFG");
   sendBinary(FOOTER);
   delay(1000);
 }
 
 // function to send the given integer value to the serial port
 void sendBinary(unsigned int value) {
-  //Serial.print("Entire word: ");
-  //Serial.println(value, HEX);
-  // send the two bytes that comprise a two byte (16 bit) integer
-  Serial.write(lowByte(value));   // send the low byte
-  //Serial.print("Low byte: ");
-  //Serial.println(lowByte(value), HEX);
   Serial.write(highByte(value));  // send the high byte
-  //Serial.print("High byte: ");
-  //Serial.println(highByte(value), HEX);
+  Serial.write(lowByte(value));   // send the low byte
 }
 
 // function to send the given long integer value to the serial port
 void sendBinary(unsigned long value) {
-  // first send the low 16 bit integer value
-  unsigned int temp = value & 0xFFFF;  // get the value of the lower 16 bits
+  // send the higher 16 bit integer value:
+  unsigned temp = value >> 16; 
   sendBinary(temp);
-  //Serial.print("Lower: ");
-  //Serial.println(temp, HEX);
-  // then send the higher 16 bit integer value:
-  temp = value >> 16;  // get the value of the higher 16 bits
+  temp = value & 0xFFFF;
+  // send the low 16 bit integer value
   sendBinary(temp);
-  //Serial.print("Upper: "); 
-  //Serial.println(temp, HEX);
 }
 
 unsigned long readBinary() {
