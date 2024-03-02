d5208e19aee496ba641f23814013b2c803c91883
Steve Cirelli <scirelli+git@gmail.com>
2024-03-01 19:04:38 -0500

    quick save

--- src/arduino/ham_hw_comms/ham_hw_comms.ino
+++ src/arduino/ham_hw_comms/ham_hw_comms.ino
@@ -1,6 +1,6 @@
 /*
- * SendBinary sketch
- * Sends a header followed by two random integer values as binary data.
+ * HAM HW Commuinication
+ *
 */
 #include <util/crc16.h>
 //int 16bits
@@ -33,28 +33,60 @@
 #define CMD_COLOR_GREEN_IDX 6
 // Motor Command
 #define CMD_MOTOR_IDX       3
-#define CMD_MAX_SZ          16
 
 #define CMD_OPCODE_MOTOR    67  // C
 #define CMD_OPCODE_LED      73  // I
 
-#define CMD_STATE_HEADER_SEARCH  1
-#define CMD_STATE_OPCODE         2
-#define CMD_STATE_READ_MOTOR_CMD 3
-#define CMD_STATE_READ_PIXEL_CMD 4
-#define CMD_STATE_RESET          5
+#define CMD_MAX_SZ          16
+#define CMD_HEADER_SZ       4
+#define CMD_CRC_SZ          2
+#define CMD_OPCODE_SZ       1
 
+#define CMD_MOTOR_PARAM_SZ  1
+#define CMD_MOTOR_CMD_SZ    (CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_MOTOR_PARAM_SZ)
+
+#define CMD_PIXEL_PARAM_SZ  4
+#define CMD_PIXEL_CMD_SZ    (CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_PIXEL_PARAM_SZ)
+
+#define CMD_STATE_HEADER_SEARCH  1
+#define CMD_STATE_READ_CRC       2
+#define CMD_STATE_READ_OPCODE    3
+#define CMD_STATE_READ_MOTOR_CMD 4
+#define CMD_STATE_READ_PIXEL_CMD 5
+#define CMD_STATE_RESET          6
+#define CMD_STATE_EXEC           7
+
+/*
+Basic layout                Example Motor command              Example Pixel Command
+┏━━━━━━━━━━┓                  ┏━━━━━━━━━━━━━┓                  ┏━━━━━━━━━━┓
+┃ HEADER   ┃ 4 bytes          ┃   HEADER    ┃ 4 bytes          ┃ HEADER   ┃ 4 bytes
+┠──────────┨                  ┠─────────────┨                  ┠──────────┨
+┃ CRC      ┃ 2 bytes          ┃   CRC       ┃ 2 bytes          ┃ CRC      ┃ 2 bytes
+┠──────────┨                  ┠─────────────┨                  ┠──────────┨
+┃ OPCODE   ┃ 1 byte           ┃   67        ┃ 1 byte           ┃ 73       ┃ 1 byte
+┠──────────┨                  ┠─────────────┨                  ┠──────────┨
+┃ PARAM 1  ┃ 1 byte           ┃ 0b00000001  ┃ 1 byte (Pins)    ┃ 1        ┃ 1 byte (Segment index)
+┠──────────┨                  ┗━━━━━━━━━━━━━┛                  ┠──────────┨
+┃ PARAM N  ┃ N bytes                                           ┃ 0xFF     ┃ 1 byte (red)
+┗━━━━━━━━━━┛                                                   ┠──────────┨
+                                                               ┃ 0x00     ┃ 1 byte (green)
+                                                               ┠──────────┨
+                                                               ┃ 0x0F     ┃ 1 byte (blue)
+                                                               ┗━━━━━━━━━━┛
+*/
+//------------------------------------
 
 const uint32_t TX_DELIM = 0xDEADBEAF; // 222 173 190 175
 uint16_t msg[CONTENT_SZ];
 
 //--- RX Command Data ---
-const uint32_t CMD_DELIM = 0x54455645; // STEVE Temp for testing
+const uint32_t CMD_DELIM = 0x53544556; // STEV Temp for testing
 uint8_t commandBuffer[CMD_MAX_SZ+1]; //+1 for debugging purposes. I'd like to print as a string, last char needs to be \0
 uint32_t cmdDelim = 0;
 int cmdReadState = CMD_STATE_HEADER_SEARCH;
-int cmdStartIdx = 0;
-int commandBufIdx = 0;
+int cmdStartIdx      = 0;
+int parsedByteCount  = 0;
+int commandBufIdx    = 0;
 //----------------------
 
 void setup() {
@@ -73,7 +105,7 @@ void setup() {
 
 void loop() {
   //sendMessage();
-  cmd_parseCommand();
+  cmd_parseCommands();
   delay(10);
 }
 
@@ -110,61 +142,86 @@ void sendBinary(uint32_t value) {
 }
 
 // Note: Serial.read has a 64byte buffer
-uint32_t cmd_parseCommand() {
+uint32_t cmd_parseCommands() {
+    cmd_processStates();
+}
+
+void cmd_processStates() {
     uint8_t c;
-
-    if (Serial.available()) {
-        c = Serial.read();
-        cmd_insertIntoBuffer(c);
-
-        switch(cmdReadState) {
-            case CMD_STATE_HEADER_SEARCH:
+    switch(cmdReadState) {
+        case CMD_STATE_HEADER_SEARCH:
+            if (Serial.available()) {
                 debugPrintln("Searching for header...");
-                cmd_headerSearch(c);
+                cmd_headerSearch(cmd_insertIntoBuffer(Serial.read())); //read() returns -1 if buffer is empty
                 debugPrintData();
-                break;
-            case CMD_STATE_OPCODE:
+            }
+            break;
+        case CMD_STATE_READ_CRC:
+            if (Serial.available()) {
+                debugPrintln("Parsing CRC...");
+                cmd_parseCRC(cmd_insertIntoBuffer(Serial.read()));
+                debugPrintData();
+            }
+            break;
+        case CMD_STATE_READ_OPCODE:
+            if (Serial.available()) {
                 debugPrintln("Parsing OpCode...");
+                c = cmd_insertIntoBuffer(Serial.read());
                 cmd_parseOptCode(c);
                 debugPrintData();
-                break;
-            case CMD_STATE_READ_MOTOR_CMD:
+            }
+            break;
+        case CMD_STATE_READ_MOTOR_CMD:
+            if (Serial.available()) {
                 debugPrintln("Parsing Motor Cmd...");
-                cmd_parseMotorCmd(c);
+                cmd_parseMotorCmd(cmd_insertIntoBuffer(Serial.read()));
                 debugPrintData();
-                break;
-            case CMD_STATE_READ_PIXEL_CMD:
+            }
+            break;
+        case CMD_STATE_READ_PIXEL_CMD:
+            if (Serial.available()) {
                 debugPrintln("Parsing Pixel Cmd...");
-                cmd_parsePixelCmd(c);
+                cmd_parsePixelCmd(cmd_insertIntoBuffer(Serial.read()));
                 debugPrintData();
-                break;
-            case CMD_STATE_CRC:
-                debugPrintln("Parsing CRC...");
-                cmd_parseCRC(c);
-                debugPrintData();
-                break;
-            case CMD_STATE_RESET:
-                debugPrintln("Resetting command parser");
-                cmd_reset();
-                debugPrintData();
-                break;
-            default: //Invalid state
-                debugPrintln("Invalid command state");
-                cmd_reset();
-                debugPrintData();
-        }
+            }
+            break;
+        case CMD_STATE_VALIDATE_CMD:
+            break;
+        case CMD_STATE_EXEC:
+            Serial.println("Executing command");
+            cmdReadState = CMD_STATE_RESET;
+            break;
+        case CMD_STATE_RESET:
+            debugPrintln("Resetting command parser");
+            cmd_reset();
+            debugPrintData();
+            break;
+        default: //Invalid state
+            debugPrintln("Invalid command state");
+            cmd_reset();
+            debugPrintData();
     }
 }
 
 void cmd_headerSearch(uint8_t c) {
     cmdDelim = (cmdDelim << 8) | c;
     if(cmdDelim == CMD_DELIM){
-        cmdReadState = CMD_STATE_OPCODE;
+        parsedByteCount = CMD_HEADER_SZ;
+        cmdReadState = CMD_STATE_READ_CRC;
+    }
+}
+
+void cmd_parseCRC(uint8_t c) {
+    parsedByteCount++;
+    if(parsedByteCount == CMD_HEADER_SZ + CMD_CRC_SZ){
+        cmdReadState = CMD_STATE_READ_OPCODE;
+    }else if (parsedByteCount > CMD_HEADER_SZ + CMD_CRC_SZ) {
+        cmdReadState = CMD_STATE_RESET;
     }
 }
 
 void cmd_parseOptCode(uint8_t c) {
-    cmdStartIdx = commandBufIdx;
+    parsedByteCount++;
     switch(c) {
     case CMD_OPCODE_MOTOR:
         cmdReadState = CMD_STATE_READ_MOTOR_CMD;
@@ -179,23 +236,51 @@ void cmd_parseOptCode(uint8_t c) {
 }
 
 void cmd_parseMotorCmd(uint8_t c) {
-    cmdReadState = CMD_STATE_RESET;
+    parsedByteCount++;
+    if(parsedByteCount == CMD_HEADER_SZ + CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_MOTOR_PARAM_SZ) {
+        cmdReadState = CMD_STATE_VALIDATE_CMD;
+    }else if(parsedByteCount > CMD_HEADER_SZ + CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_MOTOR_PARAM_SZ) {
+        cmdReadState = CMD_STATE_RESET;
+    }
 }
 
 void cmd_parsePixelCmd(uint8_t c) {
-    cmdReadState = CMD_STATE_RESET;
+    parsedByteCount++;
+    if(parsedByteCount == CMD_HEADER_SZ + CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_PIXEL_PARAM_SZ) {
+        cmdReadState = CMD_STATE_VALIDATE_CMD;
+    }else if(parsedByteCount > CMD_HEADER_SZ + CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_PIXEL_PARAM_SZ) {
+        cmdReadState = CMD_STATE_RESET;
+    }
 }
 
-void cmd_parseCRC(uint8_t c) {
-    cmdReadState = CMD_STATE_OPCODE;
+void cmd_validateCRC() {
+    if(cmd_isValidateCRC()) {
+        cmdReadState = CMD_STATE_EXEC;
+    }else{
+        cmdReadState = CMD_STATE_RESET;
+    }
+}
+
+int cmd_getCmdLength() {
+  int t = commandBufIdx;
+  if(commandBufIdx < cmdStartIdx) {
+    t += CMD_MAX_SZ;
+  }
+  Serial.print("Cmd Length: "); Serial.println(t);
+
+  return t - cmdStartIdx;
+}
+
+int cmd_isValidateCRC() {
 }
 
 uint8_t cmd_insertIntoBuffer(uint8_t c) {
-    commandBuffer[commandBufIdx++%CMD_MAX_SZ] = c;
+    commandBuffer[commandBufIdx++ %CMD_MAX_SZ] = c;
     return c;
 }
 
 void cmd_reset() {
+    parsedByteCount ^= parsedByteCount;
     commandBufIdx ^= commandBufIdx;
     cmdStartIdx ^= cmdStartIdx;
     cmdDelim ^= cmdDelim;
