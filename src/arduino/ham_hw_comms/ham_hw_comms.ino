5456ae8b532fae9a05f852c2dfe711dc06972690
Steve Cirelli <scirelli+git@gmail.com>
2024-03-08 20:36:26 -0500

    quick save

--- src/arduino/ham_hw_comms/ham_hw_comms.ino
+++ src/arduino/ham_hw_comms/ham_hw_comms.ino
@@ -4,8 +4,12 @@
 */
 #include <stdbool.h>
 #include <util/crc16.h>
+#include <stdarg.h>
+
 //int 16bits
 //long 32 bits
+
+#define ENABLE_LOGGING
 
 //--- Transmit message layout --- 24 bytes including CRC and TX_DELIM. Word indexes
 #define IR_FRONT_LEFT_IDX     0
@@ -49,18 +53,24 @@
 
 #define CMD_MOTOR_PARAM_SZ  1
 #define CMD_MOTOR_CMD_SZ    (CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_MOTOR_PARAM_SZ)
+#define CMD_MOTOR_BREAK  0
+#define CMD_MOTOR_STOP   1
+#define CMD_MOTOR_CCW    2
+#define CMD_MOTOR_CW     3
 
 #define CMD_PIXEL_PARAM_SZ  5
 #define CMD_PIXEL_CMD_SZ    (CMD_CRC_SZ + CMD_OPCODE_SZ + CMD_PIXEL_PARAM_SZ)
 
-#define CMD_STATE_HEADER_SEARCH  1
+#define CMD_STATE_HEADER_SEARCH  0
+#define CMD_STATE_HEADER_FOUND   1
 #define CMD_STATE_READ_CRC       2
 #define CMD_STATE_READ_OPCODE    3
 #define CMD_STATE_READ_MOTOR_CMD 4
 #define CMD_STATE_READ_PIXEL_CMD 5
 #define CMD_STATE_VALIDATE_CMD   6
 #define CMD_STATE_RESET          7
-#define CMD_STATE_EXEC           8
+#define CMD_STATE_CHECK_OPCODE   8
+#define CMD_NUMBER_OF_STATES     9
 
 /*
 Basic layout                Example Motor command              Example Pixel Command
@@ -91,6 +101,28 @@ const uint32_t TX_DELIM = 0xDEADBEAF; // 222 173 190 175
 uint16_t msg[CONTENT_SZ];
 
 //--- RX Command Data ---
+void cmd_parseCommands();
+void cmd_processStates();
+void cmd_headerSearch();
+void cmd_headerFound();
+void cmd_parseCRC();
+void cmd_validateCRC();
+void cmd_parseOptCode();
+void cmd_parseMotorCmd();
+void cmd_parsePixelCmd();
+void cmd_reset();
+void cmd_checkOpCode();
+void cmd_executeMotorCmd();
+void cmd_executePixelCmd();
+int cmd_getCmdLength();
+uint8_t cmd_getOpCode();
+uint16_t cmd_getCRC();
+uint8_t cmd_insertIntoBuffer();
+bool cmd_isValidateCRC();
+
+//--- Types ---
+typedef bool (*handler_t)();
+//--- Data  ---
 const uint32_t CMD_DELIM = 0x53544556; // STEV Temp for testing
 uint8_t commandBuffer[CMD_MAX_SZ+1]; //+1 for debugging purposes. I'd like to print as a string, last char needs to be \0
 uint32_t cmdDelim = 0;
@@ -98,6 +130,17 @@ int cmdReadState = CMD_STATE_HEADER_SEARCH;
 int cmdStartIdx      = 0;
 int parsedByteCount  = 0;
 int commandBufIdx    = 0;
+handler_t handlers[] = {
+    [CMD_STATE_HEADER_SEARCH] = &cmd_headerSearch,
+    [CMD_STATE_HEADER_FOUND] = &cmd_headerFound,
+    [CMD_STATE_READ_CRC] = &cmd_parseCRC,
+    [CMD_STATE_READ_OPCODE] = &cmd_parseOptCode,
+    [CMD_STATE_READ_MOTOR_CMD] = &cmd_parseMotorCmd,
+    [CMD_STATE_READ_PIXEL_CMD] = &cmd_parsePixelCmd,
+    [CMD_STATE_VALIDATE_CMD] = &cmd_validateCRC,
+    [CMD_STATE_RESET] = &cmd_reset,
+    [CMD_STATE_CHECK_OPCODE] = &cmd_checkOpCode
+};
 //----------------------
 
 void setup() {
@@ -152,45 +195,19 @@ void sendBinary(uint32_t value) {
   sendBinary(temp);
 }
 
+// ============== Commands ========================
 // Note: Serial.read has a 64byte buffer
-uint32_t cmd_parseCommands() {
+void cmd_parseCommands() {
     cmd_processStates();
 }
 
 void cmd_processStates() {
-    uint8_t c;
-    switch(cmdReadState) {
-        case CMD_STATE_HEADER_SEARCH:
-            cmd_headerSearch();
-            break;
-        case CMD_STATE_READ_CRC:
-            cmd_parseCRC();
-            break;
-        case CMD_STATE_READ_OPCODE:
-            cmd_parseOptCode();
-            break;
-        case CMD_STATE_READ_MOTOR_CMD:
-            cmd_parseMotorCmd();
-            break;
-        case CMD_STATE_READ_PIXEL_CMD:
-            cmd_parsePixelCmd();
-            break;
-        case CMD_STATE_VALIDATE_CMD:
-            cmd_validateCRC();
-            break;
-        case CMD_STATE_EXEC:
-            Serial.println("Executing command");
-            cmdReadState = CMD_STATE_RESET;
-            break;
-        case CMD_STATE_RESET:
-            log(LOG_DEBUG, "Resetting command parser");
-            cmd_reset();
-            logData();
-            break;
-        default: //Invalid state
-            log(LOG_DEBUG, "Invalid command state");
-            cmd_reset();
-            logData();
+    if(cmdReadState >= 0 && cmdReadState < CMD_NUMBER_OF_STATES){
+        (*handlers[cmdReadState])();
+    }else{
+        log(LOG_DEBUG, "Invalid command state");
+        cmd_reset();
+        logData();
     }
 }
 
@@ -202,11 +219,17 @@ void cmd_headerSearch() {
         cmdDelim = (cmdDelim << 8) | c;
         if(cmdDelim == CMD_DELIM){
             log(LOG_DEBUG, "Header found!");
-            cmd_reset();
-            cmdReadState = CMD_STATE_READ_CRC;
+            cmdReadState = CMD_STATE_HEADER_FOUND;
         }
         logData();
     }
+}
+
+void cmd_headerFound() {
+    log(LOG_DEBUG, "Resetting buffer to fill with command.");
+    cmd_reset();
+    log(LOG_DEBUG, "Changing state to read the CRC.");
+    cmdReadState = CMD_STATE_READ_CRC;
 }
 
 void cmd_parseCRC() {
@@ -242,9 +265,8 @@ void cmd_parseOptCode() {
             break;
         default:
             log(LOG_DEBUG, "Invalid op code");
-            cmd_reset();
+            cmdReadState = CMD_STATE_RESET;
         }
-        logData();
     }
 }
 
@@ -279,14 +301,83 @@ void cmd_parsePixelCmd() {
     }
 }
 
+void cmd_checkOpCode() {
+    log(LOG_DEBUG, "Checking op code");
+    switch(cmd_getOpCode()) {
+        case CMD_OPCODE_MOTOR:
+            log(LOG_DEBUG, "Executing motor command");
+            cmd_executeMotorCmd();
+            break;
+        case CMD_OPCODE_LED:
+            log(LOG_DEBUG, "Executing led command");
+            cmd_executePixelCmd();
+            break;
+        default:
+            cmdReadState = CMD_STATE_RESET;
+    }
+}
+
+void cmd_executeMotorCmd() {
+    uint8_t param = commandBuffer[CMD_CRC_SZ + CMD_OPCODE_SZ]; //Skip the CRC
+    Serial.print("Motor param: "); Serial.println(param);
+    switch(param) {
+    case CMD_MOTOR_CW:
+        cmd_motorCW();
+        break;
+    case CMD_MOTOR_CCW:
+        cmd_motorCCW();
+        break;
+    case CMD_MOTOR_BREAK:
+        cmd_motorBreak();
+        break;
+    case CMD_MOTOR_STOP:
+        cmd_motorStop();
+        break;
+    default:
+        cmdReadState = CMD_STATE_RESET;
+    }
+}
+
+void cmd_motorCW() {
+    log(LOG_DEBUG, "Executing motor cw.");
+    cmdReadState = CMD_STATE_RESET;
+}
+
+void cmd_motorCCW() {
+    log(LOG_DEBUG, "Executing motor ccw.");
+    cmdReadState = CMD_STATE_RESET;
+}
+
+void cmd_motorStop() {
+    log(LOG_DEBUG, "Executing motor stop.");
+    cmdReadState = CMD_STATE_RESET;
+}
+
+void cmd_motorBreak() {
+    log(LOG_DEBUG, "Executing motor break.");
+    cmdReadState = CMD_STATE_RESET;
+}
+
+void cmd_executePixelCmd() {
+    cmdReadState = CMD_STATE_RESET;
+}
+
 void cmd_validateCRC() {
     log(LOG_DEBUG, "Validating CRC");
     if(cmd_isValidateCRC()) {
         log(LOG_DEBUG, "Valid CRC");
-        cmdReadState = CMD_STATE_EXEC;
+        cmdReadState = CMD_STATE_CHECK_OPCODE;
     }else{
         cmdReadState = CMD_STATE_RESET;
     }
+}
+
+uint8_t cmd_getOpCode(){
+    return commandBuffer[CMD_CRC_SZ]; //Skip the CRC
+}
+
+uint16_t cmd_getCRC(){
+    return commandBuffer[0];
 }
 
 int cmd_getCmdLength() {
@@ -314,22 +405,46 @@ uint8_t cmd_insertIntoBuffer(uint8_t c) {
 }
 
 void cmd_reset() {
+    log(LOG_DEBUG, "Resetting command parser");
     parsedByteCount ^= parsedByteCount;
     commandBufIdx ^= commandBufIdx;
     cmdStartIdx ^= cmdStartIdx;
     cmdDelim ^= cmdDelim;
     cmdReadState = CMD_STATE_HEADER_SEARCH;
+    logData();
 }
 
 void cmd_printBuffer() {
     log("Debug", (const char*)commandBuffer);
 }
 
+#ifdef ENABLE_LOGGING
 size_t log(const char* type, const char* str) {
     size_t d = Serial.print("##");
     d += Serial.print(type);
     d += Serial.print(":");
     d += Serial.println(str);
+    return d;
+}
+
+size_t log(const char *type, unsigned int noParams, ...) {
+    size_t d = 0;
+    const char *str;
+
+    va_list ptr;
+    va_start(ptr, noParams);
+
+    d += Serial.print("##");
+    d += Serial.print(type);
+    d += Serial.print(":");
+    for(unsigned int i=0; i<noParams - 1; i++) {
+        str = va_arg(ptr, const char*);
+        d += Serial.print(str);
+    }
+    str = va_arg(ptr, const char*);
+    d += Serial.println(str);
+
+    va_end(ptr);
     return d;
 }
 
@@ -342,3 +457,14 @@ size_t logData() {
     d += Serial.println("'");
     return d;
 }
+#else
+size_t log(const char* type, const char* str) {
+    return 0;
+}
+size_t log(const char *type, unsigned int noParams, ...) {
+    return 0;
+}
+size_t logData() {
+    return 0;
+}
+#endif
